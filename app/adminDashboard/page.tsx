67b359e38bfa93524c9fd69101070d21131d4548
NahuelRaimondi <nahuraimondi@gmail.com>
2026-04-26 16:33:31 -0300

    login y log out funcionando

--- app/adminDashboard/page.tsx
+++ app/adminDashboard/page.tsx
@@ -1,5 +1,11 @@
+import Nav from "../ui/nav";
+
 export default async function AdminDashboard() {
     return (
-        <h1>ola soy nawel soy admin y pienso que las rubias son tontas</h1>
+        <>
+            <h1>ola soy nawel soy admin y pienso que las rubias son tontas</h1>
+            <Nav />
+        </>
+
     );
 }
---
+++ app/ui/nav.tsx
@@ -0,0 +1,16 @@
+import { signOut } from "@/auth"
+import { Button} from "./button";
+
+export default function Nav() {
+
+    return (
+        <form
+            action={async () => {
+                'use server';
+                await signOut({ redirectTo: '/' });
+            }}
+        >
+            <Button className="cursor-pointer hover:bg-indigo-300 border-solid border-1 active:bg-blue-600 aria-disabled:cursor-not-allowed aria-disabled:opacity-50" buttonText={"Cerrar sesion"} />
+        </form>
+    )
+}
