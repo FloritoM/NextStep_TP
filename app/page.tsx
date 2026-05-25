c889c5ef89847cc11d991e1b63bc7dc689f96d71
Florencia Martinez <martinez.florencia90@gmail.com>
2026-05-25 17:36:56 -0300

    estructura y estilos de landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,171 @@
 import SignInButton from "./ui/signInButton";
+import SignUpButton from "./ui/SignUpButton";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser, faUserTie } from '@fortawesome/free-solid-svg-icons'
+import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div>
-      <h1>Pagina Inicial</h1>
-      <SignInButton />
+    <div className="h-auto">
+      <section id="inicio" className="h-screen bg-main">
+        <header className="sticky top-0 w-full">
+          <div className="bg-[url('/landing-header.png')] bg-cover">
+            <h1 className="text-center text-[4rem] text-black font-bold py-5">Next Step</h1>
+            <p className="px-17 py-5 text-black font-semibold text-[1.5rem] text-center">Simplificamos los procesos de selección</p>
+            <p className="px-17 py-5 text-black font-semibold text-[1.5rem] text-center">Mejor experiencia para recruiters y candidatos</p>
+          </div>
+          <div className=" flex justify-center pr-4 border border-gray-700">
+            <nav className="w-[100%]">
+              <ul className="flex justify-center gap-10 text-white text-[1.5rem] bg-gray-700">
+                <li><a href="#inicio">Inicio</a></li>
+                <li><a href="#nosotros">Nosotros</a></li>
+                <li><a href="#contacto">Contacto</a></li>
+              </ul>
+            </nav>
+          </div>
+        </header>
+
+        <div>
+          <div className="flex justify-evenly">
+            <div className="flex items-center border border-gray-700 w-[45%] rounded-xl bg-gray-700">
+              <FontAwesomeIcon icon={faUserTie} className="text-amber-600 text-[3rem] pl-4" />
+              <p className="px-8 py-3 text-white text-[1.3rem]"><span className="font-semibold">Recruiter:</span> puede publicar una búsqueda laboral, reciba postulantes y
+                cargue feedback estructurado para cada candidato.</p>
+            </div>
+            <div className="flex items-center border border-gray-700 w-[45%] rounded-xl bg-gray-700">
+              <FontAwesomeIcon icon={faUser} className="text-yellow-400 text-[3rem] pl-4" />
+              <p className="px-8 py-3 text-white text-[1.3rem]"><span className="font-semibold">Candidato:</span> puede visualizar el estado de su postulación y recibir devoluciones
+                útiles para seguir mejorando.</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex justify-center items-center mt-12">
+          <div className="w-[70%] flex justify-center items-center rounded-xl border border-gray-700 bg-gray-800/50 py-25">
+            <div id="buttons" className="w-[30%] rounded-xl border border-gray-700 flex flex-col justify-center items-center bg-[url('/buttons-section.png')] bg-cover">
+              <div className="m-[8%]">
+                <SignInButton />
+              </div>
+              <div className="mb-[8%]">
+                <SignUpButton />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section id="nosotros" className="h-full bg-main">
+        <div>
+          <p className="px-17 py-5 text-white font-semibold text-[1.5rem]">Empresas que trabajan con nosotros</p>
+          <div className="carousel">
+            <div className="group">
+              <div className="card">
+                <Image
+                  src="/carousel_images/meli.jpg"
+                  alt="meli"
+                  width={300}
+                  height={200}
+                  loading="eager"
+                />
+              </div>
+              <div className="card">
+                <Image
+                  src="/carousel_images/ford.jpg"
+                  alt="ford"
+                  width={300}
+                  height={200}
+                />
+              </div>
+              <div className="card">
+                <Image
+                  src="/carousel_images/blizz.jpg"
+                  alt="blizzard"
+                  width={300}
+                  height={200}
+                />
+              </div>
+              <div className="card">
+                <Image
+                  src="/carousel_images/ypf.jpg"
+                  alt="ypf"
+                  width={300}
+                  height={200}
+                />
+              </div>
+              <div className="card">
+                <Image
+                  src="/carousel_images/samsung.jpg"
+                  alt="samsung"
+                  width={300}
+                  height={200}
+                />
+              </div>
+              <div className="card">
+                <Image
+                  src="/carousel_images/riot.jpg"
+                  alt="riot"
+                  width={300}
+                  height={200}
+                />
+              </div>
+            </div>
+
+            <div aria-hidden className="group">
+              <div className="card">
+                <Image
+                  src="/carousel_images/meli.jpg"
+                  alt="meli"
+                  width={300}
+                  height={200}
+                  loading="eager"
+                />
+              </div>
+              <div className="card">
+                <Image
+                  src="/carousel_images/ford.jpg"
+                  alt="meli"
+                  width={300}
+                  height={200}
+                />
+              </div>
+              <div className="card">
+                <Image
+                  src="/carousel_images/blizz.jpg"
+                  alt="meli"
+                  width={300}
+                  height={200}
+                />
+              </div>
+              <div className="card">
+                <Image
+                  src="/carousel_images/ypf.jpg"
+                  alt="meli"
+                  width={300}
+                  height={200}
+                />
+              </div>
+              <div className="card">
+                <Image
+                  src="/carousel_images/samsung.jpg"
+                  alt="meli"
+                  width={300}
+                  height={200}
+                />
+              </div>
+              <div className="card">
+                <Image
+                  src="/carousel_images/riot.jpg"
+                  alt="meli"
+                  width={300}
+                  height={200}
+                />
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </section>
+
     </div>
   );
 }
--- app/ui/SignUpButton.tsx
+++ app/ui/SignUpButton.tsx
@@ -1,7 +1,7 @@
 import { redirect } from "next/navigation"
 import { Button } from "./button";
 
-export default function SignInButton() {
+export default function SignUpButton() {
     return (
         <form
             action={async () => {
@@ -9,7 +9,7 @@ export default function SignInButton() {
                 redirect('/login' );
             }}
         >
-            <Button className="cursor-pointer border-none bg-yellow-400 rounded-lg active:bg-blue-600 text-xl text-black font-semibold hover:bg-amber-600" buttonText={"Iniciar sesion"} />
+            <Button className="cursor-pointer border-none bg-main rounded-lg text-xl text-white font-semibold hover:bg-yellow-400" buttonText={"Registrarse"} />
         </form>
     )
 }
