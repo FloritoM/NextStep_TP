37c39f32a4928d123199240c0ae674485817f543
LauEroles <laura.eroles@gmail>
2026-06-04 17:06:43 -0300

    feature register: app/register/page file added

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,5 +1,18 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import RegisterModal from "@/components/auth/RegisterModal";
+
 export default function RegisterPage() {
+  const router = useRouter();
+
+  function handleClose() {
+    router.push("/");
+  }
+
   return (
-    <div>ola!!!</div>
-  )
+    <div>
+      <RegisterModal onClose={handleClose} />
+    </div>
+  );
 }
