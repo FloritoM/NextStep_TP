eb0a3b2777f1df253d8768280bbbe3495c97b670
Melissa <lissak230@gmail.com>
2026-06-15 02:50:57 -0300

    update navbar

--- app/ui/Navbar.tsx
+++ app/ui/Navbar.tsx
@@ -3,6 +3,7 @@ import SignOutButton from './signOutButton';
 import NextStepIcon from '@/components/icon';
 import { auth } from '@/auth';
 import { User } from '../lib/definitions';
+import { canApplyToJobOffer } from '../lib/permissions';
 
 export default async function Navbar() {
     const session = await auth();
@@ -12,26 +13,26 @@ export default async function Navbar() {
     return (
         <nav className="sticky top-0 z-40 bg-gray-800 border-b border-gray-700 shadow-lg px-10 py-4 flex justify-between items-center">
             <NextStepIcon />
-
-            <ul className="flex gap-10 text-[1.2rem] font-semibold text-gray-300">
-                <li>
-                    <Link href="/home" className="hover:text-amber-500 transition-colors text-amber-500">
-                        Inicio
-                    </Link>
-                </li>
-                <li>
-                    <Link href={`/${role}/dashboard`} className="hover:text-amber-500 transition-colors">
-                        Dashboard
-                    </Link>
-                </li>
-                <li>
-                    <Link href="/profile" className="hover:text-amber-500 transition-colors">
-                        Perfil
-                    </Link>
-                </li>
-            </ul>
-
-            <div className="flex place-items-center justify-end pr-4">
+            <div className="flex items-center gap-5 md:gap-10">
+                <ul className="flex gap-5 md:gap-10 text-[1.2rem] font-semibold text-gray-300">
+                    <li>
+                        {canApplyToJobOffer(user) && (
+                            <Link href="/applicant/jobs" className="hover:text-amber-500 transition-colors">
+                                Vacantes
+                            </Link>
+                        )}
+                    </li>
+                    <li>
+                        <Link href={`/${role}/dashboard`} className="hover:text-amber-500 transition-colors">
+                            Dashboard
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/profile" className="hover:text-amber-500 transition-colors">
+                            Perfil
+                        </Link>
+                    </li>
+                </ul>
                 <SignOutButton />
             </div>
         </nav>
